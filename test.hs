7453aaf50bef9b6831baf6f863a695e2029454f2
Susan Brander <snoozins@gmail.com>
2018-07-02 21:52:31 +1000

    Chapter 2.5 - basic functions'

--- test.hs
+++ test.hs
@@ -1,3 +1,10 @@
 sayHello :: String -> IO ()
 sayHello x =
     putStrLn ("Hello, " ++ x ++ "!")
+
+triple x = x * 3
+half x = x /2
+square x = x*x
+
+prelude = 3.14
+areaOfCircle r = prelude * (r * r)
