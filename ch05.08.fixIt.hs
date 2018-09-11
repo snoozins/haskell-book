0b6f234fe7ce44f641682791c9113a7e6a0a4c9a
Susan Brander <snoozins@gmail.com>
2018-09-11 20:02:10 +1000

    Chapter 5.8 Completed - Type Exercises

--- ch05.08.fixIt.hs
+++ ch05.08.fixIt.hs
@@ -1,7 +1,16 @@
 module Sing where
 
+--fstString :: [Char] ++ [Char]
 fstString :: [Char] -> [Char]
 fstString x = x ++ "in the rain"
 
+--sndString :: [Char] -> Char
 sndString :: [Char] -> [Char]
-sndString = undefined 
+sndString x = x ++ " over the rainbow"
+
+sing :: [Char]
+--sing = if (x > y) then fstString x else sndString y 
+sing = if (x < y) then fstString x else sndString y 
+ where
+    x = "Singing"
+    y = "Somewhere"
