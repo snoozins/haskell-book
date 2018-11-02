be55ac8d7534390e5cf5ca8b2b8b7ce38765188c
Susan Brander <snoozins@gmail.com>
2018-11-02 12:55:46 +1100

    refactor of Ch9.6 Question1

--- ch09.06.symmetry.hs
+++ ch09.06.symmetry.hs
@@ -4,19 +4,7 @@ myTestString = "sheryl wants fun"
 
 myWords :: String -> [String]
 myWords x = go x []
-
-go :: String -> [String] -> [String]
-go string list 
-    | length string < 1 = list
-    | otherwise = (takeWord string) : go (dropWord string) list
-
-takeWord :: String -> String
-takeWord input = takeWhile (/= ' ') input
-
-dropWord :: String -> String
-dropWord input = conditionalDropLeadingSpace ( dropWhile (/= ' ') input)
-    
-conditionalDropLeadingSpace :: String -> String
-conditionalDropLeadingSpace input
-    | (take 1 input) == " " = tail input
-    | otherwise = input
+    where go string list 
+            | length string < 1 = list
+            | otherwise = (takeWhile (/= ' ') string) 
+                : go (drop 1 ( dropWhile (/= ' ') string)) list
