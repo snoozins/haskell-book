bbb0502da307df43b24e1c3c59d0709b05caceaa
Susan Brander <snoozins@gmail.com>
2018-11-02 12:58:17 +1100

    keeping history of learning

--- ch09.06.symmetry.hs
+++ ch09.06.symmetry.hs
@@ -2,6 +2,45 @@ module Symmetry where
 
 myTestString = "sheryl wants fun"
 
+{- 
+myWords :: String -> [String]
+myWords x = go x []
+
+go :: String -> [String] -> [String]
+go string list 
+    | length string < 1 = list
+    | otherwise = (takeWord string) : go (dropWord string) list
+
+takeWord :: String -> String
+takeWord input = takeWhile (/= ' ') input
+
+dropWord :: String -> String
+dropWord input = conditionalDropLeadingSpace ( dropWhile (/= ' ') input)
+    
+conditionalDropLeadingSpace :: String -> String
+conditionalDropLeadingSpace input
+    | (take 1 input) == " " = tail input
+    | otherwise = input
+
+became 
+
+myWords :: String -> [String]
+myWords x = go x []
+
+go :: String -> [String] -> [String]
+go string list 
+    | length string < 1 = list
+    | otherwise = (takeWord string) : go (dropWord string) list
+
+takeWord :: String -> String
+takeWord input = takeWhile (/= ' ') input
+
+dropWord :: String -> String
+dropWord input = drop 1 ( dropWhile (/= ' ') input)
+    
+became
+-}
+
 myWords :: String -> [String]
 myWords x = go x []
     where go string list 
