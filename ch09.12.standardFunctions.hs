411adf8b33c46c01fdd7f5979b8032a4767f7382
Susan Brander <snoozins@gmail.com>
2018-11-21 17:18:07 +1100

    Completed Chapter 9.12 Standard Functions - could do with some refactoring

--- ch09.12.standardFunctions.hs
+++ ch09.12.standardFunctions.hs
@@ -26,3 +26,42 @@ myElem' :: Eq a => a -> [a] -> Bool
 myElem' _ [] = False
 myElem' a xs = myAny (==a) xs
 
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse (x : xs) = (myReverse xs) ++ x : []
+
+squish :: [[a]] -> [a]
+squish [] = []
+squish (x : xs) =  x ++ squish xs
+
+squishMap :: (a -> [b]) -> [a] -> [b]
+squishMap _ [] = []
+squishMap f (x : xs) = f x ++ squishMap f xs
+
+squishAgain :: [[a]] -> [a]
+squishAgain [] = []
+squishAgain (x : xs) = x ++ squishMap id xs
+
+
+myMaximumBy :: (a -> a -> Ordering) -> [a] -> a
+myMaximumBy f xs = go f xs (head xs)
+    where go f xs max
+            | length xs == 0 = max 
+            | f (head xs) max == GT = go f (tail xs) (head xs)
+            | otherwise = go f (tail xs) max
+
+myMinimumBy :: (a -> a -> Ordering) -> [a] -> a
+myMinimumBy f (x : xs) = go x xs x
+    where go a (x : xs) max
+            | length xs == 0 = getSmallest x max
+            | otherwise = go x xs (getSmallest a max)
+          getSmallest a b
+            | f a b == LT = a
+            | otherwise = b 
+
+
+myMaximum :: (Ord a) => [a] -> a
+myMaximum xs = myMaximumBy compare xs
+
+myMinimum :: (Ord a) => [a] -> a
+myMinimum xs = myMinimumBy compare xs
